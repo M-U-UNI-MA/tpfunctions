f37cc4694d176f9980ac41d524d75fa098c437fe
M-U-UNI-MA <muckert@mail.uni-mannheim.de>
2019-01-08 14:21:59 +0100

    changes

--- R/xpdf_functions.R
+++ R/xpdf_functions.R
@@ -10,10 +10,23 @@ xpdf_meta <- function(file) {
   doc.id <- gsub(".pdf", "", basename(file), fixed = TRUE)
   file <- paste0('"', file, '"')
 
+  trim <- function(x) stringi::stri_trim_both(str = x)
+  sub  <- function(x, start, stop) stringi::stri_sub(str = x, from = start , to = stop)
+
+  suppressWarnings(
   x <- system(paste("pdfinfo -rawdates", file), intern = TRUE, wait = FALSE)
-  cols <- stringi::stri_trim_both(unlist(lapply(x, function(y) stringi::stri_sub(y, 1 , 16))))
-  cols <- stringi::stri_trans_tolower(gsub(":", "", cols, fixed = TRUE))
-  vals <- unlist(lapply(x, function(y) stringi::stri_sub(y, 17, nchar(y))))
+  )
+  if (any(stringi::stri_detect(x, fixed = "Syntax Warning: May not be a PDF file"))) {
+    cols <- "corrupted"
+    vals <- 1
+  } else if (any(stringi::stri_detect(x, fixed = "Command Line Error: Incorrect password"))) {
+    cols <- "pw_protected"
+    vals <- 1
+  } else {
+    cols <- trim(unlist(lapply(x, function(y) sub(y, 1 , 16))))
+    cols <- stringi::stri_trans_tolower(gsub(":", "", cols, fixed = TRUE))
+    vals <- unlist(lapply(x, function(y) sub(y, 17, nchar(y))))
+  }
   table <- tibble::as_tibble(matrix(c(doc.id, vals), 1, ncol = length(vals) + 1))
   colnames(table) <- c("doc_id", cols)
   return(table)
